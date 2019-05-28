cbcb06e0dd06e652ffda8074bb921c37a3256ea3
diomededavid <david@d2creativedesign.com>
2019-05-27 21:11:08 -0700

    Configurable toc

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Import stylesheets
 import './style.css';
 
-var tableOfContents = function () {
-  // Get all of the headings
-  let headings = document.querySelectorAll('h2');
-  let toc = document.querySelector('#toc');
+var tableOfContents = function (options) {
+
+
+  let defaults = {
+    selector: 'h2',
+    target: '#toc',
+    addLinks: true,
+    heading: 'Table of Contents'
+  }
+
+  let headings, toc, settings;
 
   /**
    * Get the heading ID (or create one if it doesn't have one)
@@ -20,14 +27,43 @@ var tableOfContents = function () {
     return elem.id;
   }
 
-  let tocItems = Array.prototype.map.call(headings, function (heading) {
-    return `<li><a href="#${getID(heading)}">${heading.textContent}</a></li>`;
-  });
+  var createTOC = function () {
+    let tocItems = Array.prototype.map.call(headings, function (heading) {
+      if (settings.addLinks) {
+        return `<li><a href="#${getID(heading)}">${heading.textContent}</a></li>`;
+      }
+      return `<li>${heading.textContent}</li>`;
 
-  if (toc && tocItems.length > 0) {
-    toc.innerHTML = `<h2>Table of Contents</h2>
-  <ul> ${tocItems.join('')}</ul>`
-  };
+
+    });
+
+    if (toc && tocItems.length > 0) {
+      toc.innerHTML = `<h2>${settings.heading}</h2>
+      <ul> ${tocItems.join('')}</ul>`
+    };
+  }
+
+  //
+  // Intits an Event Listeners
+  //
+
+  // Merge user options into defaults
+  settings = Object.assign({}, defaults, options);
+
+  // Get all of the headings
+  let headings = document.querySelectorAll(settings.selector);
+  let toc = document.querySelector(settings.target);
+
+
+  // Create the table of Contentss
+  createTOC();
 }
 
-tableOfContents();
+
+
+tableOfContents({
+  heading: 'Contents',
+  selector: 'h3',
+  target: '#contents',
+  addLinks: false,
+});
